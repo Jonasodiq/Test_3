228d41d6000252194e351891861130408f3a5533
Jonas Niyazson <jonas.niyazson@studerande.karlskogafhs.se>
2024-11-17 21:13:08 +0100

    Added A_Game

--- app/src/main/java/com/example/test_3/ActivityGame.kt
+++ app/src/main/java/com/example/test_3/ActivityGame.kt
@@ -6,6 +6,8 @@ import android.animation.ObjectAnimator
 import android.app.AlertDialog
 import android.os.Bundle
 import android.view.View
+import android.view.animation.AccelerateInterpolator
+import android.view.animation.DecelerateInterpolator
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
@@ -25,6 +27,9 @@ class ActivityGame : AppCompatActivity() {
     private var score = 0
     private var currentCardIndex = 0
 
+    private lateinit var cardFront: ImageView
+    private lateinit var cardContainer: CardView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
@@ -38,48 +43,16 @@ class ActivityGame : AppCompatActivity() {
         val guessClubButton = findViewById<ImageView>(R.id.clubsIcon)
         val guessSpadeButton = findViewById<ImageView>(R.id.spadesIcon)
         val backButton = findViewById<Button>(R.id.backButton)
-        val cardContainer = findViewById<CardView>(R.id.cardImageContainer)
-        val cardFront = findViewById<ImageView>(R.id.cardFront)
-        val cardBack = findViewById<ImageView>(R.id.cardBack)
-
-        // Initialt visa baksidan av kortet och dölja framsidan
-        cardBack.visibility = View.VISIBLE
-        cardFront.visibility = View.GONE
-
-        // Lyssnare för när användaren trycker på kortet
-        cardContainer.setOnClickListener {
-            // Rotera kortets framsida och baksida
-            val rotateFront = ObjectAnimator.ofFloat(cardFront, "rotationY", 0f, 180f)
-            val rotateBack = ObjectAnimator.ofFloat(cardBack, "rotationY", 0f, 180f)
-
-            // Sätt varaktigheten för animationen
-            rotateFront.duration = 500
-            rotateBack.duration = 500
-
-            // För att synkronisera rotationen, starta dem samtidigt
-            rotateFront.start()
-            rotateBack.start()
-
-            // När båda animationerna är klara, ändra synligheten
-            rotateFront.addListener(object : AnimatorListenerAdapter() {
-                override fun onAnimationEnd(animation: Animator) {
-                    // När rotationen är klar, växla synligheten mellan framsidan och baksidan
-                    if (cardBack.visibility == View.VISIBLE) {
-                        cardBack.visibility = View.GONE
-                        cardFront.visibility = View.VISIBLE
-                    } else {
-                        cardFront.visibility = View.GONE
-                        cardBack.visibility = View.VISIBLE
-                    }
-                    // Återställ rotationen för nästa animation
-                    cardFront.rotationY = 0f
-                    cardBack.rotationY = 0f
-                }
-            })
-        }
+        cardContainer = findViewById(R.id.cardImageContainer)
+        cardFront = findViewById(R.id.cardFront)
 
         // Visa ett första slumpmässigt kort
-        updateCardImage(cardFront)
+        updateCardImage()
+
+        // Kortanimering
+        cardContainer.setOnClickListener {
+            flipAnimation()
+        }
 
         // Gissning: Röd (Hjärter, Ruter)
         guessRedButton.setOnClickListener {
@@ -88,7 +61,7 @@ class ActivityGame : AppCompatActivity() {
             } else {
                 updateScore(-1, scoreTextView)
             }
-            updateCardImage(cardFront)
+            updateCardImage()
         }
 
         // Gissning: Svart (Klöver, Spader)
@@ -98,7 +71,7 @@ class ActivityGame : AppCompatActivity() {
             } else {
                 updateScore(-1, scoreTextView)
             }
-            updateCardImage(cardFront)
+            updateCardImage()
         }
 
         // Gissning: Hjärter
@@ -108,7 +81,7 @@ class ActivityGame : AppCompatActivity() {
             } else {
                 updateScore(-1, scoreTextView)
             }
-            updateCardImage(cardFront)
+            updateCardImage()
         }
 
         // Gissning: Ruter
@@ -118,27 +91,27 @@ class ActivityGame : AppCompatActivity() {
             } else {
                 updateScore(-1, scoreTextView)
             }
-            updateCardImage(cardFront)
+            updateCardImage()
         }
 
         // Gissning: Klöver
         guessClubButton.setOnClickListener {
             if (isGuessCorrect("Klöver")) {
-                updateScore(5, scoreTextView)
+                updateScore(+5, scoreTextView)
             } else {
                 updateScore(-1, scoreTextView)
             }
-            updateCardImage(cardFront)
+            updateCardImage()
         }
 
         // Gissning: Spader
         guessSpadeButton.setOnClickListener {
             if (isGuessCorrect("Spader")) {
-                updateScore(5, scoreTextView)
+                updateScore(+5, scoreTextView)
             } else {
                 updateScore(-1, scoreTextView)
             }
-            updateCardImage(cardFront)
+            updateCardImage()
         }
 
         // Tillbaka-knappen
@@ -150,7 +123,7 @@ class ActivityGame : AppCompatActivity() {
     /**
      * Uppdatera bilden för kortet
      */
-    private fun updateCardImage(cardFront: ImageView) {
+    private fun updateCardImage() {
         currentCardIndex = Random.nextInt(cards.size)
         val newCard = cards[currentCardIndex]
         cardFront.setImageResource(newCard.first)
@@ -179,24 +152,58 @@ class ActivityGame : AppCompatActivity() {
         when {
             score == 21 -> endGame(true) // Spelaren vinner exakt 21
             score > 21 || score < 0 -> endGame(false)  // Spelaren förlorar
-    }
-}
-
-fun endGame(isWin: Boolean) {
-    val message = if (isWin) {
-        "Grattis! Du vann spelet med 21 poäng!"
-    } else {
-        "Spelet är över. Du förlorade eftersom din poäng ${if (score > 21) "överstiger 21" else "är under 0"}."
-    }
-
-    AlertDialog.Builder(this)
-        .setTitle(if (isWin) "Du vann!" else "Spelet över")
-        .setMessage(message)
-        .setCancelable(false) // Gör dialogen obligatorisk att avvisa
-        .setPositiveButton("Tillbaka menyn") { _, _ ->
-            finish() // Avsluta spelet och återgå till menyn
         }
-        .show()
-}
-}
+    }
 
+    /**
+     * Avsluta spelet
+     */
+    private fun endGame(isWin: Boolean) {
+        val message = if (isWin) {
+            "Grattis! Du vann spelet med 21 poäng!"
+        } else {
+            "Spelet är över. Du förlorade eftersom din poäng ${if (score > 21) "överstiger 21" else "är under 0"}."
+        }
+
+        AlertDialog.Builder(this)
+            .setTitle(if (isWin) "Du vann!" else "Spelet över")
+            .setMessage(message)
+            .setCancelable(false)
+            .setPositiveButton("Tillbaka menyn") { _, _ ->
+                finish()
+            }
+            .show()
+    }
+
+    /**
+     * Animering för kortvändning
+     */
+    private fun flipAnimation() {
+        val animeToFront = ObjectAnimator.ofFloat(cardContainer, "scaleX", 1f, 0f)
+        val animeToBack = ObjectAnimator.ofFloat(cardContainer, "scaleX", 0f, 1f)
+
+        animeToFront.interpolator = DecelerateInterpolator()
+        animeToBack.interpolator = AccelerateInterpolator()
+
+        animeToFront.duration = 1000
+        animeToBack.duration = 1000
+
+        animeToFront.addListener(object : AnimatorListenerAdapter() {
+            override fun onAnimationEnd(animation: Animator) {
+                super.onAnimationEnd(animation)
+
+                // Byt bild under animeringen
+                if (cardFront.drawable.constantState == resources.getDrawable(R.drawable.clubs_k).constantState) {
+                    cardFront.setImageResource(R.drawable.diamonds_k)
+                } else {
+                    cardFront.setImageResource(R.drawable.clubs_k)
+                }
+
+                // Starta andra delen av animationen
+                animeToBack.start()
+            }
+        })
+
+        animeToFront.start()
+    }
+}
